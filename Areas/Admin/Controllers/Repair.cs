9d1e3a84090322fe45feb889c4fb78513cc990a1
RodriiTepatzi <72983645+RodriiTepatzi@users.noreply.github.com>
2021-11-05 23:16:34 -0600

    Creating RepairController

---
+++ Areas/Admin/Controllers/Repair.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SSI_WebApp.Areas.Admin.Controllers
+{
+    public class Repair : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Orders()
+        {
+            return View();
+        }
+    }
+}
